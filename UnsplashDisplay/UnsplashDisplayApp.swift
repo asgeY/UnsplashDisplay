0244e0670e5b6ed244855e83665a88af8eaf4dba
AsgeY <asmow@yahoo.com>
2023-04-26 23:05:04 -0500

    Initial Commit

---
+++ UnsplashDisplay/UnsplashDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashDisplayApp.swift
+//  UnsplashDisplay
+//
+//  Created by AsgeY on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
